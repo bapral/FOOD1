94f45dfad2b30cefba7a1b8a03eb6ec486b82fad
bapral <bapral@gmail.com>
2026-04-12 16:17:17 +0800

    UI: Replace emojis with SVG icons and update marker shape

--- app.js
+++ app.js
@@ -67,13 +67,16 @@ function updateUserMarker(coords) {
 }
 
 function renderMarkers() {
+    const foodIconSvg = `<svg viewBox="0 0 24 24"><path d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z"/></svg>`;
+    
     foodData.forEach(item => {
         if (item.lat && item.lng) {
             const marker = L.marker([item.lat, item.lng], {
                 icon: L.divIcon({
                     className: 'food-marker-icon',
-                    iconSize: [16, 16],
-                    iconAnchor: [8, 8]
+                    html: foodIconSvg,
+                    iconSize: [24, 24],
+                    iconAnchor: [12, 12]
                 })
             }).addTo(map);
 
